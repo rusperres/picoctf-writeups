34424d6692a58e28f0547166b703cc9f0d4df977
Jairus Jasper V. Colindres <jairusjaspervc@gmail.com>
2025-08-03 22:40:32 +0800

    Delete pw_crack3/crack.py

--- pw_crack3/crack.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-# Try guessing the password
-for pw in ["password", "123456", "km81088", "ctf123", "admin", "letmein"]:
-    h = hashlib.md5(pw.encode()).digest()
-    if h.hex() == "16026d60ff9b54410b3435b403afd226":
-        print(f"[+] Match found: {pw}")
